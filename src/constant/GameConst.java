37e5489f2892155ff14d40a8c650fc5de5808969
Ian Cheng <91156531+Tun-ChungCheng@users.noreply.github.com>
2023-01-30 15:02:07 +0800

    Delete src/constant directory

--- src/constant/GameConst.java
+++
@@ -1,9 +0,0 @@
-package constant;
-
-public interface GameConst {
-    public static final int GAME_WIDTH = 1344;
-    public static final int GAME_HEIGHT = 720;
-
-    public static final int FPS = 120;
-    public static final int UPS = 200;
-}
--- src/constant/MapConst.java
+++
@@ -1,7 +0,0 @@
-package constant;
-
-public interface MapConst {
-    public static final int MAP_WIDTH = 10752;
-    public static final int MAP_HEIGHT = 720;
-
-}
